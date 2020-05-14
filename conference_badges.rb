f2597761014d1e6abebcdb4c63d02394bb460644
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-05-14 22:22:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,9 @@ end
 #assigning each speaker to one room
 def assign_rooms(attendees)
   #this started the room number out to bt 0
+  counter = 0 
+  attendees.each_with_index.collect {|speaker,room| puts "Hello, #{speaker}! You'll be assigned to room #{room}"}
+  counter += 1
       #this will add on number to the room until the speakers each have one
 end
 def printer(attendees)
